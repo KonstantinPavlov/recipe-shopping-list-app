3194565f47da6e10fd7b46d6d7ac78e24169202a
Konstantin Pavlov <konstpavlovsdr@gmail.com>
2018-02-21 21:24:02 +0300

    Simple navigation bar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,13 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  @Output() menuSelected = new EventEmitter<string>();
+
+  onSelect(menu: string) {
+    this.menuSelected.emit(menu);
+  }
 }
